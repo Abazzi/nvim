dd8e1caef0fe24d7bab4fbb17f720a8eada0b770
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-08-18 13:14:56 -0400

    added which-key entries

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -4,8 +4,9 @@ return {
     cmd = "Telescope",
     init = function ()
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>sw', builtin.grep_string, {})
     end,
 }
 
--- lua/core/plugins/whichkey.lua
+++ lua/core/plugins/whichkey.lua
@@ -4,6 +4,21 @@ return {
   init = function()
     vim.o.timeout = true
     vim.o.timeoutlen = 300
+    local wk = require("which-key")
+    wk.register({
+      ['<leader>'] = {
+        f = {
+          name = "+file",
+          f = { "<cmd>Telescope find_files<cr>", "Find File" },
+          r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+          n = { "<cmd>enew<cr>", "New File" },
+        },
+       g = {
+        name = "+git",
+          f = {"<cmd>Telescope git_files<cr>", "Search [G]it [F]iles"},
+       },
+      }
+    },opts)
   end,
   opts = {
     -- your configuration comes here
