0c826a0f7a2a0ccd8d16a10a47999801bdd964f6
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-08-19 11:42:11 -0400

    added harpoon and which_key mappings

--- lua/core/plugins/whichkey.lua
+++ lua/core/plugins/whichkey.lua
@@ -17,6 +17,31 @@ return {
         name = "+git",
           f = {"<cmd>Telescope git_files<cr>", "Search [G]it [F]iles"},
        },
+      h = {
+      name = "harpoon",
+      a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "add file" },
+      r = { "<cmd>lua require('harpoon.mark').rm_file()<cr>", "remove file" },
+      m = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "harpoon menu" },
+      n = { "<cmd>lua require('harpoon.ui').nav_next()<cr>", "next file" },
+      p = { "<cmd>lua require('harpoon.ui').nav_prev()<cr>", "previous file" },
+      ["1"] = { "<cmd> lua require('harpoon.ui').nav_file(1)<cr>", "file 1" },
+      ["2"] = { "<cmd> lua require('harpoon.ui').nav_file(2)<cr>", "file 2" },
+      ["3"] = { "<cmd> lua require('harpoon.ui').nav_file(3)<cr>", "file 3" },
+      },
+      H = {
+          name = "help/debug/conceal",
+          c = {
+              name = "conceal",
+              h = { ":set conceallevel=1<cr>", "hide/conceal" },
+              s = { ":set conceallevel=0<cr>", "show/unconceal" },
+          },
+          t = {
+              name = "treesitter",
+              t = { vim.treesitter.inspect_tree, "show tree" },
+              c = { ":=vim.treesitter.get_captures_at_cursor()<cr>", "show capture" },
+              n = { ":=vim.treesitter.get_node():type()<cr>", "show node" },
+          },
+      },
       }
     },opts)
   end,
