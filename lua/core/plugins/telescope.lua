fb7cc3d1eddcc9c378b4b3d041ef1c8394611139
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-08-20 12:05:20 -0400

    changed 'Find Git Files' keybind for Telescope

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
     init = function ()
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
     vim.keymap.set('n', '<leader>sw', builtin.grep_string, {})
     end,
 }
