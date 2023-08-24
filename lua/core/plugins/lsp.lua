0353d105ca42c54ab619f2c0bc889c93c6e04695
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-08-24 12:39:05 -0400

    changed cmp keybindings

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -32,6 +32,8 @@ return {
   nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
   nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
+  nmap('<C-j>', require('cmp').mapping.select_next_item(), 'Next Item');
+  nmap('<C-k>', require('cmp').mapping.select_prev_item(), 'Next Item');
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
   nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
@@ -117,8 +119,8 @@ cmp.setup {
     end,
   },
   mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
