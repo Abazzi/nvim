287aff972bfc3f1a9e9722064f1da8fff7c63b1d
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-09-01 11:28:07 -0400

    Harpoon commands now use the base leader key instead of 'leader+h' plugin updates removed undodir setting in set.lua set wrap to true, and textwidth to 80

--- lua/core/plugins/whichkey.lua
+++ lua/core/plugins/whichkey.lua
@@ -14,17 +14,17 @@ return {
           r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
           n = { "<cmd>enew<cr>", "New File" },
         },
-      h = {
-      name = "harpoon",
-      a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "add file" },
-      r = { "<cmd>lua require('harpoon.mark').rm_file()<cr>", "remove file" },
-      m = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "harpoon menu" },
-      n = { "<cmd>lua require('harpoon.ui').nav_next()<cr>", "next file" },
-      p = { "<cmd>lua require('harpoon.ui').nav_prev()<cr>", "previous file" },
-      ["1"] = { "<cmd> lua require('harpoon.ui').nav_file(1)<cr>", "file 1" },
-      ["2"] = { "<cmd> lua require('harpoon.ui').nav_file(2)<cr>", "file 2" },
-      ["3"] = { "<cmd> lua require('harpoon.ui').nav_file(3)<cr>", "file 3" },
-      ["4"] = { "<cmd> lua require('harpoon.ui').nav_file(4)<cr>", "file 4" },
+       ['<leader>'] ={
+          name = "harpoon",
+          a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "add file" },
+          r = { "<cmd>lua require('harpoon.mark').rm_file()<cr>", "remove file" },
+          m = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "harpoon menu" },
+          n = { "<cmd>lua require('harpoon.ui').nav_next()<cr>", "next file" },
+          p = { "<cmd>lua require('harpoon.ui').nav_prev()<cr>", "previous file" },
+          ["1"] = { "<cmd> lua require('harpoon.ui').nav_file(1)<cr>", "file 1" },
+          ["2"] = { "<cmd> lua require('harpoon.ui').nav_file(2)<cr>", "file 2" },
+          ["3"] = { "<cmd> lua require('harpoon.ui').nav_file(3)<cr>", "file 3" },
+          ["4"] = { "<cmd> lua require('harpoon.ui').nav_file(4)<cr>", "file 4" },
       },
       e = {"<cmd> lua vim.diagnostic.open_float(0,{scope='line'})<cr>", "show error"},
       H = {
@@ -42,7 +42,7 @@ return {
           },
       },
       E = {"<cmd>Explore<cr>", "Open netrw"},
-      }
+      },
     },opts)
   end,
   opts = {
--- lua/core/set.lua
+++ lua/core/set.lua
@@ -10,11 +10,11 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
+vim.opt.textwidth = 80
 
 vim.opt.swapfile = false
 vim.opt.backup = false
--- vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
