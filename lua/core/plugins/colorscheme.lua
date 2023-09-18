3a1f16c18e40dc0a1b7810de1fdd7f91601d6cfb
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-09-18 15:02:14 -0400

    added remappings to copy and paste directly to clipboard

--- lua/core/plugins/colorscheme.lua
+++ lua/core/plugins/colorscheme.lua
@@ -1,11 +1,11 @@
 return {
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin",
-  priority = 1000,
-  config = function ()
-  local cp = require 'catppuccin'
-    cp.setup()
-    vim.cmd.colorscheme 'catppuccin-macchiato'
-  end,
+	"catppuccin/nvim",
+	lazy = false,
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		local cp = require("catppuccin")
+		cp.setup()
+		vim.cmd.colorscheme("catppuccin-macchiato")
+	end,
 }
--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,4 +1,8 @@
 vim.g.mapleader = " "
-vim.keymap.set('i', "<C-l>", "<Esc>")
-vim.keymap.set('n', "<leader>rw", vim.cmd.Ex)
+vim.keymap.set("i", "<C-l>", "<Esc>")
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>yy", '"+yy')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("n", "<leader>rw", vim.cmd.Ex)
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
