fa4b0b456bbf9e590432627eb8f8e5c39c2a9dd9
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-09-19 12:54:11 -0400

    Added keybinds to open fugitive window vertically and horizontally (Prettier reformatted file which makes git think I replaced the whole file) added comments to remap.lua added keybindings to move selected lines up and down in visual

--- lua/core/plugins/whichkey.lua
+++ lua/core/plugins/whichkey.lua
@@ -1,55 +1,61 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 300
-    local wk = require("which-key")
-    wk.register({
-      ['<leader>'] = {
-        f = {
-          name = "+file",
-          g = { "<cmd>Telescope git_files<cr>", "Find Git File" },
-          f = { "<cmd>Telescope find_files<cr>", "Find File" },
-          s = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
-          r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-          n = { "<cmd>enew<cr>", "New File" },
-        },
-       h ={
-          name = "harpoon",
-          a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "add file" },
-          r = { "<cmd>lua require('harpoon.mark').rm_file()<cr>", "remove file" },
-          m = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "harpoon menu" },
-          n = { "<cmd>lua require('harpoon.ui').nav_next()<cr>", "next file" },
-          p = { "<cmd>lua require('harpoon.ui').nav_prev()<cr>", "previous file" },
-          ["1"] = { "<cmd> lua require('harpoon.ui').nav_file(1)<cr>", "file 1" },
-          ["2"] = { "<cmd> lua require('harpoon.ui').nav_file(2)<cr>", "file 2" },
-          ["3"] = { "<cmd> lua require('harpoon.ui').nav_file(3)<cr>", "file 3" },
-          ["4"] = { "<cmd> lua require('harpoon.ui').nav_file(4)<cr>", "file 4" },
-      },
-      e = {"<cmd> lua vim.diagnostic.open_float(0,{scope='line'})<cr>", "show error"},
-      H = {
-          name = "help/debug/conceal",
-          c = {
-              name = "conceal",
-              h = { ":set conceallevel=1<cr>", "hide/conceal" },
-              s = { ":set conceallevel=0<cr>", "show/unconceal" },
-          },
-          t = {
-              name = "treesitter",
-              t = { vim.treesitter.inspect_tree, "show tree" },
-              c = { ":=vim.treesitter.get_captures_at_cursor()<cr>", "show capture" },
-              n = { ":=vim.treesitter.get_node():type()<cr>", "show node" },
-          },
-      },
-      db = {"<cmd>DapToggleBreakpoint<cr>", "Debugger: Add Breakpoint at line"},
-      dr = {"<cmd> DapContinue<cr>", "Run or continue the debugger"},
-      },
-    },opts)
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 300
+		local wk = require("which-key")
+		wk.register({
+			["<leader>"] = {
+				f = {
+					name = "+file",
+					g = { "<cmd>Telescope git_files<cr>", "Find Git File" },
+					f = { "<cmd>Telescope find_files<cr>", "Find File" },
+					s = { "<cmd>Telescope live_grep<cr>", "Live Grep" },
+					r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+					n = { "<cmd>enew<cr>", "New File" },
+				},
+				h = {
+					name = "harpoon",
+					a = { "<cmd>lua require('harpoon.mark').add_file()<cr>", "add file" },
+					r = { "<cmd>lua require('harpoon.mark').rm_file()<cr>", "remove file" },
+					m = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", "harpoon menu" },
+					n = { "<cmd>lua require('harpoon.ui').nav_next()<cr>", "next file" },
+					p = { "<cmd>lua require('harpoon.ui').nav_prev()<cr>", "previous file" },
+					["1"] = { "<cmd> lua require('harpoon.ui').nav_file(1)<cr>", "file 1" },
+					["2"] = { "<cmd> lua require('harpoon.ui').nav_file(2)<cr>", "file 2" },
+					["3"] = { "<cmd> lua require('harpoon.ui').nav_file(3)<cr>", "file 3" },
+					["4"] = { "<cmd> lua require('harpoon.ui').nav_file(4)<cr>", "file 4" },
+				},
+				g = {
+					name = "+git",
+					h = { "<cmd>G<cr>", "open fugitive in horizonal window split" },
+					v = { "<cmd>vertical G<cr>", "open fugitive in vertical window split" },
+					l = { "<cmd>Git log --oneline<cr>", "git log one line" },
+				},
+				e = { "<cmd> lua vim.diagnostic.open_float(0,{scope='line'})<cr>", "show error" },
+				H = {
+					name = "help/debug/conceal",
+					c = {
+						name = "conceal",
+						h = { ":set conceallevel=1<cr>", "hide/conceal" },
+						s = { ":set conceallevel=0<cr>", "show/unconceal" },
+					},
+					t = {
+						name = "treesitter",
+						t = { vim.treesitter.inspect_tree, "show tree" },
+						c = { ":=vim.treesitter.get_captures_at_cursor()<cr>", "show capture" },
+						n = { ":=vim.treesitter.get_node():type()<cr>", "show node" },
+					},
+				},
+				db = { "<cmd>DapToggleBreakpoint<cr>", "Debugger: Add Breakpoint at line" },
+				dr = { "<cmd> DapContinue<cr>", "Run or continue the debugger" },
+			},
+		}, opts)
+	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
 }
--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,8 +1,17 @@
 vim.g.mapleader = " "
+-- Remap for Escape key
 vim.keymap.set("i", "<C-l>", "<Esc>")
+
+-- Copy and Paste to clipboard keymaps
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>yy", '"+yy')
 vim.keymap.set("n", "<leader>Y", '"+Y')
 vim.keymap.set("n", "<leader>rw", vim.cmd.Ex)
+
+-- Open Parent Directory in Oil
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- Move Selected lines up and down
+vim.keymap.set("v", "J", [[:move '>+1<CR>gv=gv]], { silent = true })
+vim.keymap.set("v", "K", [[:move '<-2<CR>gv=gv]], { silent = true })
