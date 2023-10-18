12357e2d0d01e7a71cb2d84e4c14053b6f4763cf
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-10-18 13:29:08 -0400

    Added function 'Sandr' to wrap serach and replace in neovim added spacing between harpoon keymaps

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -99,30 +99,37 @@ keybind("n", "<leader>hm",
   "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>"
   , { desc = "Open Menu" }
 )
+
 keybind("n", "<leader>ha",
   "<cmd>lua require('harpoon.mark').add_file()<cr>"
   , { desc = "Add File" }
 )
+
 keybind("n", "<leader>hr",
   "<cmd>lua require('harpoon.mark').remove_file()<cr>"
   , { desc = "Remove File" }
 )
+
 keybind("n", "<leader>hn",
   "<cmd>lua require('harpoon.ui').nav_next()<cr>"
   , { desc = "Next File" }
 )
+
 keybind("n", "<leader>hp",
   "<cmd>lua require('harpoon.ui').nav_prev()<cr>"
   , { desc = "Previous File" }
 )
+
 keybind("n", "<leader>h1",
   "<cmd>lua require('harpoon.ui').nav_file(1)<cr>"
   , { desc = "Go To File 1" }
 )
+
 keybind("n", "<leader>h2",
   "<cmd>lua require('harpoon.ui').nav_file(2)<cr>"
   , { desc = "Go To File 2" }
 )
+
 keybind("n", "<leader>h3",
   "<cmd>lua require('harpoon.ui').nav_file(3)<cr>"
   , { desc = "Go To File 3" }
