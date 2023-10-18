07718c6caa53b488464638204d6f3d05bad11445
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-10-18 12:46:37 -0400

    went back to harpoon made remap.lua look more consistent

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -2,46 +2,46 @@ vim.g.mapleader = " "
 local keybind = vim.keymap.set
 
 -- Copy and Paste to clipboard keymaps
-vim.keymap.set("n", "<leader>y", '"+y')
-vim.keymap.set("v", "<leader>y", '"+y')
-vim.keymap.set("n", "<leader>yy", '"+yy')
-vim.keymap.set("n", "<leader>Y", '"+Y')
-vim.keymap.set("n", "<leader>rw", vim.cmd.Ex)
+keybind("n", "<leader>y", '"+y')
+keybind("v", "<leader>y", '"+y')
+keybind("n", "<leader>yy", '"+yy')
+keybind("n", "<leader>Y", '"+Y')
+keybind("n", "<leader>rw", vim.cmd.Ex)
 
 -- Open Parent Directory in Oil
-vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+keybind("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
 -- Move Selected lines up and down
-vim.keymap.set("v", "J", [[:move '>+1<CR>gv=gv]], { silent = true })
-vim.keymap.set("v", "K", [[:move '<-2<CR>gv=gv]], { silent = true })
-vim.keymap.set({ "n" }, "<Leader>k", function()
-	vim.lsp.buf.signature_help()
+keybind("v", "J", [[:move '>+1<CR>gv=gv]], { silent = true })
+keybind("v", "K", [[:move '<-2<CR>gv=gv]], { silent = true })
+keybind({ "n" }, "<Leader>k", function()
+  vim.lsp.buf.signature_help()
 end, { silent = true, noremap = true, desc = "toggle signature" })
 
 -- Telescope  keybinds
 keybind("n", "<leader>fg", function()
-	require("telescope.builtin").git_files()
+  require("telescope.builtin").git_files()
 end, { desc = "Find Git File" })
 keybind("n", "<leader>ff", function()
-	require("telescope.builtin").find_files()
+  require("telescope.builtin").find_files()
 end, { desc = "Find Files" })
 keybind("n", "<leader>fw", function()
-	require("telescope.builtin").grep_string()
+  require("telescope.builtin").grep_string()
 end, { desc = "Find Git File" })
 keybind("n", "<leader>fc", function()
-	require("telescope.builtin").git_commits()
+  require("telescope.builtin").git_commits()
 end, { desc = "Find Git File" })
 keybind("n", "<leader>fg", function()
-	require("telescope.builtin").git_files()
+  require("telescope.builtin").git_files()
 end, { desc = "Find Git File" })
 keybind("n", "<leader>fs", function()
-	require("telescope.builtin").live_grep()
+  require("telescope.builtin").live_grep()
 end, { desc = "Live Grep" })
 keybind("n", "<leader>fr", function()
-	require("telescope.builtin").oldfiles()
+  require("telescope.builtin").oldfiles()
 end, { desc = "Open Recent File" })
 keybind("n", "<leader>fm", function()
-	require("telescope.builtin").marks()
+  require("telescope.builtin").marks()
 end, { desc = "Show Marks" })
 
 -- Other file related keybinds
@@ -67,29 +67,63 @@ keybind("n", "<leader>db", "<cmd>DapContinue<cr>", { desc = "Run or continue the
 
 -- Trouble Keybinds
 keybind("n", "<leader>xx", function()
-	require("trouble").open()
+  require("trouble").open()
 end, { desc = "Open Trouble" })
 
 keybind("n", "<leader>xw", function()
-	require("trouble").open("workspace_diagnostics")
+  require("trouble").open("workspace_diagnostics")
 end, { desc = "Workspace Diagnostics" })
 
 keybind("n", "<leader>xd", function()
-	require("trouble").open("document_diagnostics")
+  require("trouble").open("document_diagnostics")
 end, { desc = "Document Diagnostics" })
 
 keybind("n", "<leader>xq", function()
-	require("trouble").open("quickfix")
+  require("trouble").open("quickfix")
 end, { desc = "Quickfix Menu" })
 
 keybind("n", "<leader>xR", function()
-	require("trouble").open("lsp_references")
+  require("trouble").open("lsp_references")
 end, { desc = "LSP References" })
 
 keybind("n", "<leader>xn", function()
-	require("trouble").next({ skip_groups = true, jump = true })
+  require("trouble").next({ skip_groups = true, jump = true })
 end, { desc = "Next Item" })
 
 keybind("n", "<leader>xp", function()
-	require("trouble").previous({ skip_groups = true, jump = true })
+  require("trouble").previous({ skip_groups = true, jump = true })
 end, { desc = "Previous Item" })
+
+-- Harpoon Keybinds
+keybind("n", "<leader>hm",
+  "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>"
+  , { desc = "Open Menu" }
+)
+keybind("n", "<leader>ha",
+  "<cmd>lua require('harpoon.mark').add_file()<cr>"
+  , { desc = "Add File" }
+)
+keybind("n", "<leader>hr",
+  "<cmd>lua require('harpoon.mark').remove_file()<cr>"
+  , { desc = "Remove File" }
+)
+keybind("n", "<leader>hn",
+  "<cmd>lua require('harpoon.ui').nav_next()<cr>"
+  , { desc = "Next File" }
+)
+keybind("n", "<leader>hp",
+  "<cmd>lua require('harpoon.ui').nav_prev()<cr>"
+  , { desc = "Previous File" }
+)
+keybind("n", "<leader>h1",
+  "<cmd>lua require('harpoon.ui').nav_file(1)<cr>"
+  , { desc = "Go To File 1" }
+)
+keybind("n", "<leader>h2",
+  "<cmd>lua require('harpoon.ui').nav_file(2)<cr>"
+  , { desc = "Go To File 2" }
+)
+keybind("n", "<leader>h3",
+  "<cmd>lua require('harpoon.ui').nav_file(3)<cr>"
+  , { desc = "Go To File 3" }
+)
