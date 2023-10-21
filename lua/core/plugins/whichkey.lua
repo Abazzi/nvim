241bb390946001c95c315cc6be20b9c74ff655bc
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-10-21 14:14:35 -0400

    Added '+' infront of whichkey name

--- lua/core/plugins/whichkey.lua
+++ lua/core/plugins/whichkey.lua
@@ -1,42 +1,42 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	init = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
-		local wk = require("which-key")
-		wk.register({
-			["<leader>"] = {
-				f = {
-					name = "+telescope",
-				},
-				h = {
-					name = "+harpoon",
-				},
-				g = {
-					name = "+git",
-				},
-				H = {
-					name = "help/debug/conceal",
-					c = {
-						name = "conceal",
-					},
-				},
-				t = {
-					name = "+treesitter",
-				},
-				d = {
-					name = "+DAP",
-				},
-				x = {
-					name = "+trouble",
-				},
-			},
-		}, opts)
-	end,
-	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
-	},
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  init = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    local wk = require("which-key")
+    wk.register({
+      ["<leader>"] = {
+        f = {
+          name = "+telescope",
+        },
+        h = {
+          name = "+harpoon",
+        },
+        g = {
+          name = "+git",
+        },
+        H = {
+          name = "+help/debug/conceal",
+          c = {
+            name = "conceal",
+          },
+        },
+        t = {
+          name = "+treesitter",
+        },
+        d = {
+          name = "+DAP",
+        },
+        x = {
+          name = "+trouble",
+        },
+      },
+    }, opts)
+  end,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  },
 }
