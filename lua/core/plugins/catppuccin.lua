c72aec69a6febeb04a3944743bfcc40430557de1
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-10-28 21:17:57 -0400

    Added Catppuccin, Eyeliner, and UFO plugins to config

---
+++ lua/core/plugins/catppuccin.lua
@@ -0,0 +1,12 @@
+return {
+  "catppuccin/nvim",
+  lazy = false,
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    local cp = require 'catppuccin'
+    cp.setup()
+    vim.cmd.colorscheme 'catppuccin'
+  end,
+}
+
