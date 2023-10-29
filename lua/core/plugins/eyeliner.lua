ecaa42bb43988fad224ed83b5e5cf4b67d5d8b52
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-10-29 10:27:55 -0400

    Changed dim setting for eyeliner.nvim

--- lua/core/plugins/eyeliner.lua
+++ lua/core/plugins/eyeliner.lua
@@ -3,7 +3,7 @@ return {
   config = function()
     require 'eyeliner'.setup {
       highlight_on_key = true, -- show highlights only after keypress
-      dim = false              -- dim all other characters if set to true (recommended!)
+      dim = true               -- dim all other characters if set to true (recommended!)
     }
   end
 }
