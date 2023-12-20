9530d673076bd846c60fa303bece7acd376d0d0d
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2023-12-20 11:28:17 -0500

    Added Treesitter text objects to config telescope now ignores node_modules changed remaps so <C-f> does find_files() instead of git_files() added new category for whichkey updated lazy-lock

--- lua/core/plugins/telescope.lua
+++ lua/core/plugins/telescope.lua
@@ -1,6 +1,9 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.2",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  cmd = "Telescope",
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	cmd = "Telescope",
+	opts = {
+		file_ignore_patterns = { "node_modules", ".git" },
+	},
 }
