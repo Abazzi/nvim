e6cefbd6f871bf9af3a120faa4e9fbc13c24a31a
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-02-05 16:08:50 -0500

    Added Navigation for gitsigns plugin

--- lua/core/plugins/gitsigns.lua
+++ lua/core/plugins/gitsigns.lua
@@ -3,4 +3,24 @@ return {
   config = function()
     require('gitsigns').setup()
   end
+        -- Navigation
+        map("n", "]c", function()
+          if vim.wo.diff then
+            return "]c"
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
+
+        map("n", "[c", function()
+          if vim.wo.diff then
+            return "[c"
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return "<Ignore>"
+        end, { expr = true })
 }
