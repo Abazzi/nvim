045c864f59aa21153c808a20f40747e9900db513
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-02-06 11:58:17 -0500

    Forgot to delete duplicate mappings

--- lua/core/plugins/gitsigns.lua
+++ lua/core/plugins/gitsigns.lua
@@ -32,30 +32,6 @@ return {
 					return "<Ignore>"
 				end, { expr = true })
 
-<<<<<<< HEAD
-				-- Actions
-				map("n", "<leader>gH", gs.stage_hunk, { desc = "GitSigns stage hunk" })
-				map("n", "<leader>rH", gs.reset_hunk, { desc = "GitSigns reset hunk" })
-				map("v", "<leader>sH", function()
-					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end, { desc = "GitSigns stage_hunk" })
-				map("v", "<leader>rh", function()
-					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-				end, { desc = "GitSigns reset_hunk" })
-				map("n", "<leader>gU", gs.undo_stage_hunk, { desc = "GitSigns undo_stage_hunk" })
-				map("n", "<leader>sb", gs.stage_buffer, { desc = "GitSigns stage_buffer" })
-				map("n", "<leader>rb", gs.reset_buffer, { desc = "GitSigns reset_buffer" })
-				map("n", "<leader>ph", gs.preview_hunk, { desc = "GitSigns preview_hunk" })
-				map("n", "<leader>gb", function()
-					gs.blame_line({ full = true })
-				end, { desc = "GitSigns blame line" })
-				map("n", "<leader>tb", gs.toggle_current_line_blame, { desc = "GitSigns toggle blame" })
-				map("n", "<leader>gd", gs.diffthis, { desc = "GitSigns diffthis" })
-				map("n", "<leader>hD", function()
-					gs.diffthis("~")
-				end, { desc = "GitSigns diffthis" })
-				map("n", "<leader>htd", gs.toggle_deleted, { desc = "GitSigns toggle_deleted" })
-
 				-- Actions
 				map("n", "<leader>gH", gs.stage_hunk, { desc = "GitSigns state hunk" })
 				map("n", "<leader>gR", gs.reset_hunk, { desc = "GitSigns reset hunk" })
@@ -78,7 +54,6 @@ return {
 					gs.diffthis("~")
 				end, { desc = "GitSigns diffthis" })
 				map("n", "<leader>htd", gs.toggle_deleted, { desc = "GitSigns toggle_deleted" })
->>>>>>> efe90e3 (Fixed keybind)
 
 				-- Text object
 				map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", { desc = "GitSigns select hunk" })
