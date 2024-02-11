f20ad19f5a38c974b8e8108e31ae0ce850057cc4
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-02-11 14:12:59 -0500

    Updated Keybinds, removed keybind function

--- lua/core/plugins/gitsigns.lua
+++ lua/core/plugins/gitsigns.lua
@@ -1,63 +1,63 @@
 return {
-  "lewis6991/gitsigns.nvim",
-  config = function()
-    require("gitsigns").setup({
-      on_attach = function(bufnr)
-        local gs = package.loaded.gitsigns
+	"lewis6991/gitsigns.nvim",
+	config = function()
+		require("gitsigns").setup({
+			on_attach = function(bufnr)
+				local gs = package.loaded.gitsigns
 
-        local function map(mode, l, r, opts)
-          opts = opts or {}
-          opts.buffer = bufnr
-          vim.keymap.set(mode, l, r, opts)
-        end
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
 
-        -- Navigation
-        map("n", "]c", function()
-          if vim.wo.diff then
-            return "]c"
-          end
-          vim.schedule(function()
-            gs.next_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true })
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						return "]c"
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true })
 
-        map("n", "[c", function()
-          if vim.wo.diff then
-            return "[c"
-          end
-          vim.schedule(function()
-            gs.prev_hunk()
-          end)
-          return "<Ignore>"
-        end, { expr = true })
+				map("n", "[c", function()
+					if vim.wo.diff then
+						return "[c"
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return "<Ignore>"
+				end, { expr = true })
 
-        -- Actions
-        map("n", "<leader>gH", gs.stage_hunk, { desc = "GitSigns stage hunk" })
-        map("n", "<leader>rH", gs.reset_hunk, { desc = "GitSigns reset hunk" })
-        map("v", "<leader>sH", function()
-          gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, { desc = "GitSigns stage_hunk" })
-        map("v", "<leader>rh", function()
-          gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-        end, { desc = "GitSigns reset_hunk" })
-        map("n", "<leader>uH", gs.undo_stage_hunk, { desc = "GitSigns undo_stage_hunk" })
-        map("n", "<leader>sB", gs.stage_buffer, { desc = "GitSigns stage_buffer" })
-        map("n", "<leader>rB", gs.reset_buffer, { desc = "GitSigns reset_buffer" })
-        map("n", "<leader>pH", gs.preview_hunk, { desc = "GitSigns preview_hunk" })
-        map("n", "<leader>gb", function()
-          gs.blame_line({ full = true })
-        end, { desc = "GitSigns blame line" })
-        map("n", "<leader>tB", gs.toggle_current_line_blame, { desc = "GitSigns toggle blame" })
-        map("n", "<leader>gD", gs.diffthis, { desc = "GitSigns diffthis" })
-        map("n", "<leader>hD", function()
-          gs.diffthis("~")
-        end, { desc = "GitSigns diffthis" })
-        map("n", "<leader>htd", gs.toggle_deleted, { desc = "GitSigns toggle_deleted" })
+				-- Actions
+				map("n", "<leader>gH", gs.stage_hunk, { desc = "GitSigns stage hunk" })
+				map("n", "<leader>rH", gs.reset_hunk, { desc = "GitSigns reset hunk" })
+				map("v", "<leader>sH", function()
+					gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "GitSigns stage_hunk" })
+				map("v", "<leader>rh", function()
+					gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+				end, { desc = "GitSigns reset_hunk" })
+				map("n", "<leader>uH", gs.undo_stage_hunk, { desc = "GitSigns undo_stage_hunk" })
+				map("n", "<leader>sB", gs.stage_buffer, { desc = "GitSigns stage_buffer" })
+				map("n", "<leader>rB", gs.reset_buffer, { desc = "GitSigns reset_buffer" })
+				map("n", "<leader>pH", gs.preview_hunk, { desc = "GitSigns preview_hunk" })
+				map("n", "<leader>gb", function()
+					gs.blame_line({ full = true })
+				end, { desc = "GitSigns blame line" })
+				map("n", "<leader>tB", gs.toggle_current_line_blame, { desc = "GitSigns toggle blame" })
+				map("n", "<leader>gD", gs.diffthis, { desc = "GitSigns diffthis" })
+				map("n", "<leader>hD", function()
+					gs.diffthis("~")
+				end, { desc = "GitSigns diffthis" })
+				map("n", "<leader>htd", gs.toggle_deleted, { desc = "GitSigns toggle_deleted" })
 
-        -- Text object
-        map({ "o", "x" }, "iH", ":<C-U>Gitsigns select_hunk<CR>", { desc = "GitSigns select hunk" })
-      end,
-    })
-  end,
+				-- Text object
+				map({ "o", "x" }, "iH", ":<C-U>Gitsigns select_hunk<CR>", { desc = "GitSigns select hunk" })
+			end,
+		})
+	end,
 }
--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,29 +1,28 @@
 vim.g.mapleader = " "
-local keybind = vim.keymap.set
 
 -- Copy and Paste to clipboard keymaps
-keybind("n", "<leader>y", '"+y')
-keybind("v", "<leader>y", '"+y')
-keybind("n", "<leader>yy", '"+yy')
-keybind("n", "<leader>Y", '"+Y')
-keybind("n", "<leader>rw", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>yy", '"+yy')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+vim.keymap.set("n", "<leader>rw", vim.cmd.Ex)
 
 -- Paste without overwriting register
-keybind("v", "p", '"_dP')
+vim.keymap.set("v", "p", '"_dP')
 
 -- Move Selected lines up and down
-keybind("v", "J", [[:move '>+1<CR>gv=gv]], { silent = true })
-keybind("v", "K", [[:move '<-2<CR>gv=gv]], { silent = true })
-keybind({ "n" }, "<Leader>k", function()
-  vim.lsp.buf.signature_help()
+vim.keymap.set("v", "J", [[:move '>+1<CR>gv=gv]], { silent = true })
+vim.keymap.set("v", "K", [[:move '<-2<CR>gv=gv]], { silent = true })
+vim.keymap.set({ "n" }, "<Leader>k", function()
+	vim.lsp.buf.signature_help()
 end, { silent = true, noremap = true, desc = "toggle signature" })
 
 -- Other file related keybinds
-keybind("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
-keybind({ "n" }, "<Leader>k", function()
-  vim.lsp.buf.signature_help()
+vim.keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+vim.keymap.set({ "n" }, "<Leader>k", function()
+	vim.lsp.buf.signature_help()
 end, { silent = true, noremap = true, desc = "toggle signature" })
 
 --Help/Debug/Conceal Keybinds
-keybind("n", "<leader>Hch", ":set conceallevel=1<cr>", { desc = "hide/conceal" })
-keybind("n", "<leader>Hcs", ":set conceallevel=0<cr>", { desc = "show/unconceal" })
+vim.keymap.set("n", "<leader>Hch", ":set conceallevel=1<cr>", { desc = "hide/conceal" })
+vim.keymap.set("n", "<leader>Hcs", ":set conceallevel=0<cr>", { desc = "show/unconceal" })
