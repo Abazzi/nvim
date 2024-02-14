1f1ee74d66ffe1652fec025107b111f4197c282b
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-02-14 13:36:15 -0500

    Updated lualine config
    close #32

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -6,8 +6,9 @@ return {
     options = {
       icons_enabled = true,
       theme = 'catppuccin',
-      component_separators = '|',
-      section_separators = '',
+      padding = 1,
+      section_separators = { left = '', right = '' },
+      component_separators = { left = '', right = '' }
     },
   },
 }
