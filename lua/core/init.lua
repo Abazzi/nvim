e5e8f767b1067cc96e225de8514ef5904898ba07
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-02-19 13:31:52 -0500

    Went back to catppuccin

--- lua/core/init.lua
+++ lua/core/init.lua
@@ -19,9 +19,4 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " " -- Mapping space as the leading key
 
 require("lazy").setup("core.plugins")
-
-vim.o.background = "dark"
-vim.cmd("colorscheme gruvbox")
-require("gruvbox").setup({
-	contrast = "hard",
-})
+vim.cmd.colorscheme("catppuccin")
--- lua/core/plugins/colorscheme.lua
+++ lua/core/plugins/colorscheme.lua
@@ -1,6 +1,6 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+	"catppuccin/nvim",
+	name = "catppuccin",
 	priority = 1000,
-	config = true,
 	lazy = false,
 }
