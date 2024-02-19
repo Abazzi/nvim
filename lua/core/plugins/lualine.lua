174fe0169a19a79fb2aab58342cf428c02ba50b6
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-02-19 13:32:44 -0500

    Changed lualine colorscheme

--- lua/core/plugins/lualine.lua
+++ lua/core/plugins/lualine.lua
@@ -1,14 +1,14 @@
 return {
-  -- Set lualine as statusline
-  'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
-  opts = {
-    options = {
-      icons_enabled = true,
-      theme = 'gruvbox',
-      padding = 1,
-      section_separators = { left = '', right = '' },
-      component_separators = { left = '', right = '' }
-    },
-  },
+	-- Set lualine as statusline
+	"nvim-lualine/lualine.nvim",
+	-- See `:help lualine.txt`
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "catppuccin",
+			padding = 1,
+			section_separators = { left = "", right = "" },
+			component_separators = { left = "", right = "" },
+		},
+	},
 }
