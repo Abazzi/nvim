fdf4d9b3903b017de5ccee96dcc3cf64d0f500b3
Adam Bazzi <8884041+Abazzi@users.noreply.github.com>
2024-05-30 12:09:49 -0400

    Updated pugins and removed eslint for typescript

--- lua/core/plugins/trouble.lua
+++ lua/core/plugins/trouble.lua
@@ -1,39 +1,36 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    --
-    -- Trouble Keybinds
-    vim.keymap.set("n", "<leader>xx", function()
-      require("trouble").open()
-    end, { desc = "Open Trouble" }),
-
-    vim.keymap.set("n", "<leader>xw", function()
-      require("trouble").open("workspace_diagnostics")
-    end, { desc = "Workspace Diagnostics" }),
-
-    vim.keymap.set("n", "<leader>xd", function()
-      require("trouble").open("document_diagnostics")
-    end, { desc = "Document Diagnostics" }),
-
-    vim.keymap.set("n", "<leader>xq", function()
-      require("trouble").open("quickfix")
-    end, { desc = "Quickfix Menu" }),
-
-    vim.keymap.set("n", "<leader>xR", function()
-      require("trouble").open("lsp_references")
-    end, { desc = "LSP References" }),
-
-    vim.keymap.set("n", "<leader>xn", function()
-      require("trouble").next({ skip_groups = true, jump = true })
-    end, { desc = "Next Item" }),
-
-    vim.keymap.set("n", "<leader>xp", function()
-      require("trouble").previous({ skip_groups = true, jump = true })
-    end, { desc = "Previous Item" })
-
-  },
+	"folke/trouble.nvim",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
 }
