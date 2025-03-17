9ddc3e4dc1f5d732744b2f9743ff43f7f4524bd0
Adam Bazzi <adam@adambazzi.ca>
2025-03-17 14:44:54 -0400

    removed un-used plugins

--- lua/core/plugins/fugitive.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'tpope/vim-fugitive',
-  -- Fugitive Keybinds
-  vim.keymap.set("n", "<leader>gs", "<cmd>G<cr>", { desc = "Fugitive: Open Horizontal" }),
-  vim.keymap.set("n", "<leader>gv", "<cmd>vertical G<cr>", { desc = "Fugitive: Open Vertical" })
-}
