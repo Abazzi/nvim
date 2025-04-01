c7c10497668eb49681f54c94f684826689b4df89
Adam Bazzi <adam@adambazzi.ca>
2025-04-01 11:57:14 -0400

    feat: set up blink cmp
    removed nvim-cmp

--- lua/core/plugins/lsp.lua
+++ lua/core/plugins/lsp.lua
@@ -31,8 +31,6 @@ return {
 
 			nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 			nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-			nmap("<C-n>", require("cmp").mapping.select_next_item(), "Next Item")
-			nmap("<C-p>", require("cmp").mapping.select_prev_item(), "Next Item")
 			nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
 			nmap("gI", vim.lsp.buf.implementation, "[G]oto [I]mplementation")
 			nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
@@ -79,9 +77,6 @@ return {
 
 		-- Setup neovim lua configuration
 		require("neodev").setup()
-		-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
-		capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 		-- Ensure the servers above are installed
 		local mason_lspconfig = require("mason-lspconfig")
@@ -99,54 +94,6 @@ return {
 					filetypes = (servers[server_name] or {}).filetypes,
 				})
 			end,
-		})
-
-		-- [[ Configure nvim-cmp ]]
-		-- See `:help cmp`
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		require("luasnip.loaders.from_vscode").lazy_load()
-		luasnip.config.setup({})
-
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					luasnip.lsp_expand(args.body)
-				end,
-			},
-			mapping = cmp.mapping.preset.insert({
-				["<C-j>"] = cmp.mapping.select_next_item(),
-				["<C-k>"] = cmp.mapping.select_prev_item(),
-				["<C-d>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<C-Space>"] = cmp.mapping.complete({}),
-				["<CR>"] = cmp.mapping.confirm({
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				}),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_next_item()
-					elseif luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						cmp.select_prev_item()
-					elseif luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-			}),
-			sources = {
-				{ name = "nvim_lsp" },
-				{ name = "luasnip" },
-			},
 		})
 	end,
 }
