36c72a6c4523e3966df7748e93268c1b9347b179
Adam Bazzi <adam@adambazzi.ca>
2025-06-24 14:42:21 -0400

    updated packages and added blink depencendy for spelling

--- lua/core/plugins/blink.lua
+++ lua/core/plugins/blink.lua
@@ -1,7 +1,7 @@
 return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
-  dependencies = { 'rafamadriz/friendly-snippets' },
+  dependencies = { 'rafamadriz/friendly-snippets',  "archie-judd/blink-cmp-words" },
 
   -- use a release tag to download pre-built binaries
   version = '1.*',
@@ -40,6 +40,49 @@ return {
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { 'lsp', 'path', 'snippets', 'buffer' },
+      providers = {
+
+        -- Use the thesaurus source
+        thesaurus = {
+          name = "blink-cmp-words",
+          module = "blink-cmp-words.thesaurus",
+          -- All available options
+          opts = {
+            -- A score offset applied to returned items.
+            -- By default the highest score is 0 (item 1 has a score of -1, item 2 of -2 etc..).
+            score_offset = 0,
+
+            -- Default pointers define the lexical relations listed under each definition,
+            -- see Pointer Symbols below.
+            -- Default is as below ("antonyms", "similar to" and "also see").
+            pointer_symbols = { "!", "&", "^" },
+          },
+        },
+
+        -- Use the dictionary source
+        dictionary = {
+          name = "blink-cmp-words",
+          module = "blink-cmp-words.dictionary",
+          -- All available options
+          opts = {
+            -- The number of characters required to trigger completion.
+            -- Set this higher if completion is slow, 3 is default.
+            dictionary_search_threshold = 3,
+
+            -- See above
+            score_offset = 0,
+
+            -- See above
+            pointer_symbols = { "!", "&", "^" },
+          },
+        },
+      },
+
+      -- Setup completion by filetype
+      per_filetype = {
+        text = { "dictionary" },
+        markdown = { "thesaurus" },
+      },
     },
 
     -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
