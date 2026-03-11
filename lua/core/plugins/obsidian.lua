bd537d8dcb7735722c63a9fc1433daa53085452d
Adam Bazzi <adam@adambazzi.ca>
2026-03-11 11:33:23 -0400

    Staging (#57)
    * added tailwind lsp to and plugin to config
    
    * feat: updated config for snacks picker
    
    * updated packages and added blink depencendy for spelling
    
    * updated lsp to work with nvim 0.11
    
    * added obsidian plugin
    
    * Update (#56)
    
    * update plugins for nvim 0.11 update
    
    updated the following
    - blink
    - conform
    - mason
    - nvim-lspconfig
    - treesitter
    
    removed ufo folding plugin
    
    * cut: tailwind plugin
    
    added golang, eslint, and svelte to ensure_installed treesitter list

---
+++ lua/core/plugins/obsidian.lua
@@ -0,0 +1,20 @@
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- recommended, use latest release instead of latest commit
+  ft = "markdown",
+  ---@module 'obsidian'
+  ---@type obsidian.config
+  opts = {
+    legacy_commands = false, -- this will be removed in the next major release
+    workspaces = {
+      {
+        name = "school",
+        path = "~/vaults/college_notes",
+      },
+      {
+        name = "brain",
+        path = "~/vaults/brain",
+      },
+    },
+  },
+}
